af96a9542c4e3022a909b46619fa8b164ad33cf2
sofiamejiamuro <sofiamejiamuro@gmail.com>
2021-01-02 04:08:19 -0600

    adding eventListeners to dynamic selector imputs

--- js/app.js
+++ js/app.js
@@ -23,6 +23,11 @@ const showCars = (cars) => {
 createSelects = (cars) => {
     const getSpecs = cars[0];
     let specs = []
+
+    const filter = (selectDOM) => {
+        console.log(selectDOM.value);
+    };
+
     for (let spec in getSpecs) {
         specs = [...specs, spec]
 
@@ -39,16 +44,16 @@ createSelects = (cars) => {
         div.appendChild(label);
         div.appendChild(select);
 
-        const selectDOM = document.getElementById(spec)
+        const selectDOM = document.getElementById(spec);
+        selectDOM.addEventListener('change', () => filter(selectDOM))
         createOptions(cars, specs, selectDOM);
     }
-
 }
+
 const createOptions = (cars, specs, selectDOM) => {
     specs.forEach(spec => {
         if (selectDOM.id === spec) {
             const options = new Set(cars.map(car => car[spec]))
-            console.log(options);
             options.forEach((option) => {
                 const optionElement = document.createElement('option');
                 optionElement.value = option;
